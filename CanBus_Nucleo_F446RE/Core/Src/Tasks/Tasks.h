ef96b33741d3807d7fd6691831ecf7c24ab73225
Davide <ronchinidavid3@gmail.com>
2025-01-18 23:58:43 +0100

    made importing folders into the project path generic

--- CanBus_Nucleo_F446RE/Core/Src/Tasks/Tasks.h
+++ CanBus_Nucleo_F446RE/Core/Src/Tasks/Tasks.h
@@ -8,7 +8,7 @@
 
 /* TODO Defines, variables and functions to be visible (exported) to other modules that includes "Scheduler.h". */
 
-// #define DEBUG_MODE /* Define the Mode of Operation: remove the comment to set debug mode */
+#define DEBUG_MODE /* Define the Mode of Operation: remove the comment to set debug mode */
 #define ADC_BUF_LEN 2
 
 extern uint32_t adc_buf[ADC_BUF_LEN];
